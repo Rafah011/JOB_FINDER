5498d3eefc94440ae21232240b810585a8ab971f
Rafah011 <rafah011@hotmail.com>
2024-06-19 14:07:39 -0300

    Criação do models e rota para adicionar registros

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection');
+const bodyParser = require('body-parser');
 
 const PORT = 3000;
 
 app.listen(PORT, () => {
     console.log(`Esta funcionando na porta ${PORT}`);
 });
+
+// body parser
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // DB Connection
 
@@ -24,3 +29,6 @@ db
 app.get('/', (req, res) => {
     res.send('Funcinando')
 });
+
+// Jobs routes
+app.use('/jobs', require('./routes/jobs'))
---
+++ routes/jobs.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+const Job = require('../models/Job');
+
+router.get('/test', (req, res) => {
+    res.send('deu certo');
+})
+
+// add job via post
+
+router.post('/add', (req, res) => {
+
+    let {title, company, salary, email, new_job, description } = req.body;
+    
+    // insert job
+
+    Job.create({
+        title,
+        description,
+        salary,
+        company,
+        email,
+        new_job
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err));
+
+});
+
+module.exports = router
